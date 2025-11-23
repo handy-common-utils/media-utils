aeb4a04c83e2237bd777251ba5ebde28a00270e1
James Hu <james.hu.ustc@hotmail.com>
2025-11-23 19:48:12 +1100

    Cover more scenarios

--- test/get-media-info.test.ts
+++ test/get-media-info.test.ts
@@ -117,18 +117,21 @@ describe('getMediaInfo with real files', () => {
       } as MediaInfo);
     });
 
-    it.each(['engine-start.mjpeg.pcms16le.avi', 'engine-start.mpeg2video.mp2.m2ts', 'engine-start.vp9.opus.webm', 'engine-start.wmv2.wmav2.wmv'])(
-      'should fail to parse %s',
-      async (filename) => {
-        try {
-          await getMediaInfoFromFile(sampleFile(filename), { useParser: 'mp4box' });
-          expect('').toBe('should fail to parse');
-        } catch (error) {
-          expect(error).toBeInstanceOf(Error);
-          expect(error).toHaveProperty('isUnsupportedFormatError', true);
-        }
-      },
-    );
+    it.each([
+      'engine-start.mjpeg.pcms16le.avi',
+      'engine-start.h264.pcms16le.avi',
+      'engine-start.mpeg2video.mp2.m2ts',
+      'engine-start.vp9.opus.webm',
+      'engine-start.wmv2.wmav2.wmv',
+    ])('should fail to parse %s', async (filename) => {
+      try {
+        await getMediaInfoFromFile(sampleFile(filename), { useParser: 'mp4box' });
+        expect('').toBe('should fail to parse');
+      } catch (error) {
+        expect(error).toBeInstanceOf(Error);
+        expect(error).toHaveProperty('isUnsupportedFormatError', true);
+      }
+    });
   });
 
   describe('remotion parser', () => {
@@ -228,18 +231,22 @@ describe('getMediaInfo with real files', () => {
       } as MediaInfo);
     });
 
-    it.each(['engine-start.h264.mp3.mov', 'engine-start.mjpeg.pcms16le.avi', 'engine-start.mpeg2video.mp2.m2ts', 'engine-start.wmv2.wmav2.wmv'])(
-      'should fail to parse %s',
-      async (filename) => {
-        try {
-          await getMediaInfoFromFile(sampleFile(filename), { useParser: 'remotion' });
-          expect('').toBe('should fail to parse');
-        } catch (error) {
-          expect(error).toBeInstanceOf(Error);
-          expect(error).toHaveProperty('isUnsupportedFormatError', true);
-        }
-      },
-    );
+    it.each([
+      'engine-start.h264.mp3.mov',
+      'engine-start.mjpeg.pcms16le.avi',
+      'engine-start.h264.pcms16le.avi',
+      'engine-start.mpeg2video.mp2.m2ts',
+      'engine-start.wmv2.wmav2.wmv',
+    ])('should fail to parse %s', async (filename) => {
+      try {
+        await getMediaInfoFromFile(sampleFile(filename), { useParser: 'remotion' });
+        expect('').toBe('should fail to parse');
+      } catch (error) {
+        expect(error).toBeInstanceOf(Error);
+        expect(error).toHaveProperty('isUnsupportedFormatError', true);
+        // expect(error).toHaveProperty('xx', true);
+      }
+    });
   });
 
   /*
