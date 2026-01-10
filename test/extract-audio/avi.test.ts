2bf30b13b2456747df4079f5f2c1f1e778845c60
James Hu <james.hu.ustc@hotmail.com>
2026-01-10 22:43:42 +1100

    chore: cover extracting of mono pcm from avi

--- test/extract-audio/avi.test.ts
+++ test/extract-audio/avi.test.ts
@@ -117,6 +117,90 @@ describe('Extract audio from AVI', () => {
       },
     },
     {
+      filename: 'engine-start.h264.pcms16le-mono.avi',
+      expectedMediaInfo: {
+        bytesRead: 65536,
+        audioStreams: [
+          {
+            bitrate: 705600,
+            bitsPerSample: 16,
+            channelCount: 1,
+            codec: 'pcm_s16le',
+            codecDetail: 'pcm_s16le',
+            durationInSeconds: expect.closeTo(6, 0) as any,
+            id: 1,
+            sampleRate: 44100,
+            codecDetails: {
+              formatTag: 1,
+              blockAlign: 2,
+              samplesPerBlock: undefined,
+            },
+          },
+        ],
+        container: 'wav',
+        containerDetail: 'wav',
+        durationInSeconds: expect.closeTo(6, 0) as any,
+        parser: 'media-utils',
+        videoStreams: [],
+      },
+    },
+    {
+      filename: 'engine-start.h264.pcm_u8-mono.avi',
+      expectedMediaInfo: {
+        bytesRead: 65536,
+        audioStreams: [
+          {
+            bitrate: 352800,
+            bitsPerSample: 8,
+            channelCount: 1,
+            codec: 'pcm_u8',
+            codecDetail: 'pcm_u8',
+            durationInSeconds: expect.closeTo(6, 0) as any,
+            id: 1,
+            sampleRate: 44100,
+            codecDetails: {
+              formatTag: 1,
+              blockAlign: 1,
+              samplesPerBlock: undefined,
+            },
+          },
+        ],
+        container: 'wav',
+        containerDetail: 'wav',
+        durationInSeconds: expect.closeTo(6, 0) as any,
+        parser: 'media-utils',
+        videoStreams: [],
+      },
+    },
+    {
+      filename: 'engine-start.h264.adpcm_ms-mono.avi',
+      expectedMediaInfo: {
+        bytesRead: 65536,
+        audioStreams: [
+          {
+            bitrate: 177432,
+            bitsPerSample: 4,
+            channelCount: 1,
+            codec: 'adpcm_ms',
+            codecDetail: 'adpcm_ms',
+            durationInSeconds: expect.closeTo(6, 0) as any,
+            id: 1,
+            sampleRate: 44100,
+            codecDetails: {
+              formatTag: 2,
+              blockAlign: 1024,
+              samplesPerBlock: 2036,
+            },
+          },
+        ],
+        container: 'wav',
+        containerDetail: 'wav',
+        durationInSeconds: expect.closeTo(6, 0) as any,
+        parser: 'media-utils',
+        videoStreams: [],
+      },
+    },
+    {
       filename: 'engine-start.h264.mp3-mono.avi',
       expectedMediaInfo: {
         bytesRead: 48274,
